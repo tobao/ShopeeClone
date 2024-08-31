1e529106edb9348e1928a4470017ad260e0fd48b
To Nguyen Bao <tobao97@gmail.com>
2024-08-31 18:19:27 +0700

    Ui for Popover cart

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className='container'>
         <div className='grid grid-cols-1 gap-4 lg:grid-cols-3'>
           <div className='lg:col-span-1'>
-            <div>© 2022 Shopee. Tất cả các quyền được bảo lưu.</div>
+            <div>© 2024 Shopee. Tất cả các quyền được bảo lưu.</div>
           </div>
           <div className='lg:col-span-2'>
             <div>
