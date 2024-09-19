fed5adc7e806f6b7f352f90cc70dc22933b05493
To Nguyen Bao <tobao97@gmail.com>
2024-09-19 20:37:20 +0700

    Pagination/Pagination.tsx

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -3,17 +3,22 @@ import AsideFilter from './AsideFilter'
 import Product from './Product/Product'
 import SortProductList from './SortProductList'
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 import productApi from 'src/apis/product.api'
+import Pagination from 'src/components/Pagination'
 
 export default function ProductList() {
   const queryParams = useQueryParams()
+  const [page, setPage] = useState(1)
+
   const { data } = useQuery({
     queryKey: ['products', queryParams],
     queryFn: () => {
       return productApi.getProducts(queryParams)
     }
   })
-  console.log(queryParams)
+
+  console.log(data)
 
   return (
     <div className='bg-gray-200 py-6'>
@@ -32,6 +37,7 @@ export default function ProductList() {
                   </div>
                 ))}
             </div>
+            <Pagination page={page} setPage={setPage} pageSize={20} />
           </div>
         </div>
       </div>
