b514e57a0ff369b78a7c786c3e828e5e51fa908b
To Nguyen Bao <tobao97@gmail.com>
2024-10-05 10:32:38 +0700

    Đưa các component trong Productlist  vào trong một folder(ProductList/components)

--- src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
@@ -1,7 +1,7 @@
 import { Link, createSearchParams, useNavigate } from 'react-router-dom'
 import Button from 'src/components/Button'
 import path from 'src/constants/path'
-import { QueryConfig } from '../ProductList'
+import { QueryConfig } from '../../ProductList'
 import { Category } from 'src/types/category.type'
 import classNames from 'classnames'
 import InputNumber from 'src/components/InputNumber'
