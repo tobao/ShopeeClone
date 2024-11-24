6bfc4d6379754fdd14dbae19321b2565725aa848
To Nguyen Bao <tobao97@gmail.com>
2024-11-24 18:35:20 +0700

    Khai báo API purchase và fix lỗi logout chưa clear data trên React Query

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -10,6 +10,7 @@ import Profile from './pages/Profile'
 import { useContext } from 'react'
 import { AppContext } from './contexts/app.context'
 import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -58,6 +59,14 @@ export default function useRouteElements() {
           )
         }
       ]
+    },
+    {
+      path: path.cart,
+      element: (
+        <MainLayout>
+          <Cart />
+        </MainLayout>
+      )
     },
     {
       path: path.productDetail,
