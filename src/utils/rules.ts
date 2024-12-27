31ff466454a675df4091d3d102e4efe154cc738e
To Nguyen Bao <tobao97@gmail.com>
2024-12-27 20:15:18 +0700

    Code logic đổi mật khẩu (update theo version yup mới)

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -66,6 +66,15 @@ function testPriceMinMax(this: yup.TestContext<AnyObject>) {
   return price_min !== '' || price_max !== ''
 }
 
+const handleConfirmPasswordYup = (refString: string) => {
+  return yup
+    .string()
+    .required('Nhập lại password là bắt buộc')
+    .min(6, 'Độ dài từ 6 - 160 ký tự')
+    .max(160, 'Độ dài từ 6 - 160 ký tự')
+    .oneOf([yup.ref(refString)], 'Nhập lại password không khớp')
+}
+
 export const schema = yup.object({
   email: yup
     .string()
@@ -78,12 +87,7 @@ export const schema = yup.object({
     .required('Password là bắt buộc')
     .min(6, 'Độ dài từ 6 - 160 ký tự')
     .max(160, 'Độ dài từ 6 - 160 ký tự'),
-  confirm_password: yup
-    .string()
-    .required('Nhập lại password là bắt buộc')
-    .min(6, 'Độ dài từ 6 - 160 ký tự')
-    .max(160, 'Độ dài từ 6 - 160 ký tự')
-    .oneOf([yup.ref('password')], 'Nhập lại password không khớp'),
+  confirm_password: handleConfirmPasswordYup('password'),
   // Thêm .default('') vào các trường price_min và price_max trong schema. Điều này đảm bảo rằng chúng sẽ luôn có giá trị mặc định là chuỗi rỗng ('') thay vì undefined
   price_min: yup.string().default('').test({
     name: 'price-not-allowed',
@@ -104,9 +108,9 @@ export const userSchema = yup.object({
   address: yup.string().max(160, 'Độ dài tối đa là 160 ký tự'),
   avatar: yup.string().max(1000, 'Độ dài tối đa là 1000 ký tự'),
   date_of_birth: yup.date().max(new Date(), 'Hãy chọn một ngày trong quá khứ'),
-  password: schema.fields['password'],
-  new_password: schema.fields['password'],
-  confirm_password: schema.fields['confirm_password']
+  password: schema.fields['password'] as yup.StringSchema,
+  new_password: schema.fields['password'] as yup.StringSchema,
+  confirm_password: handleConfirmPasswordYup('new_password')
 })
 
 export type UserSchema = yup.InferType<typeof userSchema>
